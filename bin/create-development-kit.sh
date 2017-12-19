1612252db3cf2f5e2936175f4f31f203b1cd9f6c
msato <msato@altus5.com>
2017-12-19 11:00:37 +0900

    shellでのテンプレート処理を修正

--- bin/create-development-kit.sh
+++ bin/create-development-kit.sh
@@ -12,25 +12,30 @@ cd $rootdir
 . ./bin/config.sh
 
 # 開発チームに配布する開発キットを作成する
-./bin/render-templates.sh ./templates ./$DEVELOPMENT_KIT_NAME
+echo '開発キットを作成...'
+./bin/render-templates.sh ./templates/development-kit ./$DEVELOPMENT_KIT_NAME
 cp ./bin/download-devtool.sh ./$DEVELOPMENT_KIT_NAME/bin
 cp ./bin/setup-vbox-vagrant.sh ./$DEVELOPMENT_KIT_NAME/bin
 
 # TERASOLUNAのブランクプロジェクトを作成するために
-# starter-kitを使って開発ツールをインストールする
+# 開発ツールをインストールする
+echo '開発ツールをインストール...'
 ./$DEVELOPMENT_KIT_NAME/bin/setup-devtool.sh
 
-# starter-kitのリポジトリをプッシュする
+# 開発キットのリポジトリをプッシュする
+echo '開発キットのリポジトリをプッシュ...'
 pushd ./$DEVELOPMENT_KIT_NAME
-## starter-kitのリポジトリを作成
+## 開発キットのリポジトリを作成
 _gitlab_init_and_push $DEVELOPMENT_KIT_NAME .
 
 ## terasolunaのブランクプロジェクトを作成してプッシュする
+echo 'terasolunaのブランクプロジェクトを作成...'
 . ./bin/setenv.sh
 $rootdir/bin/setup-terasoluna.sh
+$rootdir/bin/render-templates.sh $rootdir/templates/project ./$PROJECT_NAME
 _gitlab_init_and_push $PROJECT_NAME
 
-## ブランクプロジェクトをstarter-kitのsubmoduleにする
+## ブランクプロジェクトを開発キットのsubmoduleにする
 git submodule add $PROJECT_REPO_URL $PROJECT_NAME
 git add .gitmodules
 git commit -m "Add $PROJECT_NAME as a submodule"
--- bin/render-templates.sh
+++ bin/render-templates.sh
@@ -4,6 +4,9 @@
 set -e
 trap 'echo "ERROR $0 ($LINENO)" 1>&2' ERR
 
+srcdir=$1
+dstdir=$2
+
 _ENV_VARS=(
   'GITLAB_URL'
   'GITLAB_PRIVATE_TOKEN'
@@ -24,27 +27,38 @@ _ENV_VARS=(
   'PROJECT_REPO_URL'
 )
 
+echo '' > _tmpsed
+for envvar in "${_ENV_VARS[@]}"
+do
+  value=$(eval "echo \$$envvar")
+  value=$(echo $value | sed -e 's/\(\[\|]\|[\(\)\+\?\|\$\.\*\\\^\/]\)/\\\1/g')
+  echo "s/{{ *$envvar *}}/$value/g" >> _tmpsed
+done
+
+_replace_text() {
+  text=$1
+  text=$(echo "$text" | sed -f _tmpsed)
+  echo "$text"
+}
+
+_replace_file() {
+  file=$1
+  sed -i -f _tmpsed $file
+}
+
 _render() {
   src=$1
   dst=$2
   mkdir -p $(dirname $dst)
-  text=$(cat $src)
-  for envvar in "${_ENV_VARS[@]}"
-  do
-    value=$(eval "echo \$$envvar")
-    value=$(echo $value | sed -e 's/\(\[\|]\|[\(\)\+\?\|\$\.\*\\\^\/]\)/\\\1/g')
-    text=$(echo "$text" | sed -e "s/{{ *$envvar *}}/$value/g")
-  done
-  echo "$text" > $dst
+  cp $src $dst
+  _replace_file $dst
 }
-
-srcdir=$1
-dstdir=$2
 
 if [ -d $srcdir ]; then
   for srcfile in $(find $srcdir -type f)
   do
     dstfile=$(echo $srcfile | sed -e "s|$srcdir||")
+    dstfile=$(_replace_text $dstfile)
     _render $srcfile $dstdir$dstfile
   done
 else
@@ -52,3 +66,4 @@ else
   _render $srcfile $dstdir/$(basename $srcfile)
 fi
 
+rm _tmpsed
